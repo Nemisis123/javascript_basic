080f6ec3be4e9e3386530833daaaa8c0126c9873
PiyushNegi <148423940+Nemisis123@users.noreply.github.com>
2025-04-10 11:19:37 +0000

    commit done

--- basic/01_basic.js
+++ basic/01_basic.js
@@ -1,1 +1,41 @@
-console.log("hello world");
+// const accountID = 144553; // not changeable
+// let accountEmail = "Piyush@gmail.com"; // always use this (let)
+// var accountPassword = "Piyush@123";
+// accountcity = "haldwani";
+// let accountstate;
+
+
+// // accountID = 1223456; not allowed
+// accountEmail = "pnegi@gmail.com";
+// accountPassword = "Piyush@344";
+// console.log([accountEmail,accountID,accountPassword,accountcity,accountstate]);
+ 
+// Data types is JS
+
+// let name = "piyush negi" // string
+// let age = 23 // number
+// let isloggedin = false //boolean
+
+// console.log(typeof name); // string
+// console.log(typeof age); // number
+// console.log(typeof isloggedin); // boolean
+
+// console.log(typeof null); // object
+// console.log(typeof undefined); // undefined
+// console.log(typeof symbol); // undefined
+
+// primitive data types
+// number => 1465
+// string => "piyush"
+// bigint =>3423434523534623453623463453634323
+// boolean => true/false
+// null => empty value / standalone value
+// undefined => value is not defined 
+// symbol => unique value
+
+// reference data types
+// object => collection of key value pairs
+// array => collection of items
+// function => block of code that can be called
+// date => date object
+// regex => regular expression
